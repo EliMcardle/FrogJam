2a4378c593cca3a2fb41302c4a9a40b386a5c7e0
Eli Mcardle <elimcardle2001@gmail.com>
2024-03-23 15:15:30 -0400

    Player eating works!

--- Assets/Scripts/EatableSpawner.cs
+++ Assets/Scripts/EatableSpawner.cs
@@ -6,10 +6,15 @@ using Photon.Pun;
 public class EatableSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject flyPrefab;
+    [SerializeField] private PhotonView view;
+
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("SpawnEatable", 0.5f);
+        if(PhotonNetwork.IsMasterClient)
+        {
+            Invoke("SpawnEatable", 0.5f);
+        }
 
     }
 
@@ -22,6 +27,9 @@ public class EatableSpawner : MonoBehaviour
     public void SpawnEatable()
     {
         GameObject eatable = PhotonNetwork.Instantiate(flyPrefab.name, new Vector3(Random.Range(-10f, 7f), Random.Range(1.5f, 5), 0), Quaternion.identity);
-        Invoke("SpawnEatable", 3f);
+        if(PhotonNetwork.IsMasterClient)
+        {
+            Invoke("SpawnEatable", 3f);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using Photon.Pun;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float powerDifference = 50f;
     [SerializeField] private PhotonView view;
     [SerializeField] private TadpoleController tadpole;
     [SerializeField] private FrogController frog;
@@ -23,7 +24,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(view.IsMine && Input.GetKeyDown(KeyCode.P))
+        {
+            power += 50;
+            view.RPC("SyncPower", RpcTarget.AllBuffered, power);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,8 +44,7 @@ public class PlayerController : MonoBehaviour
         if (other.tag == "Eatable" && view.IsMine)
         {
             power += other.GetComponent<Eatable>().value;
-            other.gameObject.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
-            PhotonNetwork.Destroy(other.gameObject);
+            view.RPC("DestroyObject", RpcTarget.MasterClient, other.gameObject.GetComponent<PhotonView>().ViewID);
             view.RPC("SyncPower", RpcTarget.AllBuffered, power);
         }
     }
@@ -56,10 +60,39 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && view.IsMine)
+        {
+            PlayerController otherPlayer = collision.gameObject.GetComponent<PlayerController>();
+            if(otherPlayer.power < power - powerDifference)
+            {
+                power += otherPlayer.power;
+                view.RPC("KillPlayer", RpcTarget.All, otherPlayer.gameObject.GetComponent<PhotonView>().ViewID);
+                view.RPC("SyncPower", RpcTarget.AllBuffered, power);
+            }
+        }
+    }
+
     [PunRPC]
     public void SyncPower(float myPower)
     {
         power = myPower;
         playerPowerText.text = power.ToString();
+    }
+
+    [PunRPC]
+    public void DestroyObject(int viewID)
+    {
+        PhotonNetwork.Destroy(PhotonView.Find(viewID).gameObject);
+    }
+
+    [PunRPC]
+    public void KillPlayer(int viewID)
+    {
+        PlayerController player = PhotonView.Find(viewID).gameObject.GetComponent<PlayerController>();
+        player.power = 10;
+        player.view.RPC("SyncPower", RpcTarget.AllBuffered, player.power);
+        player.transform.position = new Vector3(Random.Range(-3, 5), 0, 0);
     }
 }
