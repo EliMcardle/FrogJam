e056850038a922420281191f5b0d54512fd006aa
Eli Mcardle <elimcardle2001@gmail.com>
2024-03-24 13:47:30 -0400

    dragonfly rotation

--- Assets/Scripts/Dragonfly.cs
+++ Assets/Scripts/Dragonfly.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class Dragonfly : Eatable
 {
     [SerializeField] private float maxRange = 10f;
     [SerializeField] private float flySpeed = 7f;
+    [SerializeField] private Image bugImage;
+    [SerializeField] private PhotonView view;
     private bool flyingLeft = true;
     private Vector3 spawnPoint;
 
@@ -21,10 +25,12 @@ public class Dragonfly : Eatable
         if(transform.position.x - spawnPoint.x > maxRange)
         {
             flyingLeft = true;
+            view.RPC("ChangeSprite", RpcTarget.All, flyingLeft);
         }
         else if (transform.position.x - spawnPoint.x < -maxRange)
         {
             flyingLeft = false;
+            view.RPC("ChangeSprite", RpcTarget.All, flyingLeft);
         }
     }
 
@@ -45,6 +51,20 @@ public class Dragonfly : Eatable
         if(!other.isTrigger)
         {
             flyingLeft = !flyingLeft;
+            view.RPC("ChangeSprite", RpcTarget.All, flyingLeft);
+        }
+    }
+
+    [PunRPC]
+    public void ChangeSprite(bool state)
+    {
+        if(state)
+        {
+            bugImage.rectTransform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            bugImage.rectTransform.eulerAngles = Vector3.zero;
         }
     }
 }
